5ebc73a42c1eebcf7bdc6d300bf8cb91dbce1579
dar-ia <64155350+dar-ia@users.noreply.github.com>
2024-02-05 21:57:57 +0400

    Update Main.java

--- src/main/java/com/iriryt/Main.java
+++ src/main/java/com/iriryt/Main.java
@@ -3,10 +3,11 @@ package com.iriryt;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    private static String msg = "Master changed!!!!!!!";
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Master changed!!!!!!!");
+      
+        System.out.printf(msg);
 
        
     }
